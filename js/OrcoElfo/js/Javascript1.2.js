6833edf7d6b599c6c34579f19df3b72c89543170
David Luna González <lumiazaine@gmail.com>
2019-06-25 11:06:18 +0200

    Add files via upload

---
+++ js/OrcoElfo/js/Javascript1.2.js
@@ -0,0 +1,11 @@
+var fuerzaOrco; // Variable Orco.
+	var fuerzaElfo; // Variable Elfo.
+
+// Entrada de datos
+	
+	fuerzaOrco = prompt("introduzca el valor de la fuerza del orco");
+	fuerzaElfo = prompt("introduzca el valor de la fuerza del elfo");
+		if (fuerzaOrco == fuerzaElfo) {
+		alert ("No gana nadie")
+		}
+
