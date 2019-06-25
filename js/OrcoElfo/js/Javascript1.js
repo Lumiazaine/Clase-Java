b667ef84509efb119dc668d40f80ae5868a79907
David Luna González <lumiazaine@gmail.com>
2019-06-25 10:39:37 +0200

    Add files via upload

---
+++ js/OrcoElfo/js/Javascript1.js
@@ -0,0 +1,15 @@
+// Puedo juntar las variables en una misma linea con una "," bastánte útil. Ejemplo "var fuerzaOrco, fuerzaElfo" uwu
+//Comparaciones "=="
+//Asignaciones "="
+
+	var fuerzaOrco; // Variable Orco.
+	var fuerzaElfo; // Variable Elfo.
+
+// Entrada de datos
+	
+	fuerzaOrco=8;
+	fuerzaElfo=6;
+		if (fuerzaOrco == fuerzaElfo) {
+		alert ("No gana nadie")
+		}
+		
