423c589a5eeec52384c113bd899674265169afb6
David Luna González <lumiazaine@gmail.com>
2019-06-25 13:13:21 +0200

    Add files via upload

--- js/OrcoElfo/js/Javascript1.3.js
+++ js/OrcoElfo/js/Javascript1.3.js
@@ -1,14 +1,16 @@
 var fuerzaOrco; 		// Variable Orco.
 var fuerzaElfo; 		// Variable Elfo.
-var fuerzaPocion=0; 	// Variable Poción.
+// var fuerzaPocion=0; (Al final no se usó) 	// Variable Poción.
 var numeroAleatorio; 	// Variable Número aleatorio.
+var decisionPocima;
+
 
 // Entrada de datos
 	
 	fuerzaOrco = prompt ("introduzca el valor de la fuerza del orco");
 	fuerzaElfo = prompt ("introduzca el valor de la fuerza del elfo");
-		//como el metodo prompt devuelve un string, neceisto pasar estos valores a number.
-		// fuerzaOrco = Numbre (fuerzaOrco)
+		
+		//como el metodo prompt devuelve un string, neceisto pasar estos valores a number. (fuerzaOrco = number(fuerzaOrco)).
 
 		fuerzaOrco =Number (fuerzaOrco);
 		fuerzaElfo =Number (fuerzaElfo); 
@@ -19,8 +21,28 @@ var numeroAleatorio; 	// Variable Número aleatorio.
 			"1 - Te la tomas\n"+
 			"2 - No te la tomas");
 
-			numeroAleatorio = Math.random();
-			document.write(numeroAleatorio);
+			if (decisionPocima=1) {
+
+				numeroAleatorio = Math.random();
+
+				if (numeroAleatorio >= 0.5) {
+					fuerzaElfo = fuerzaElfo + 5;
+				}
+
+				else {
+					fuerzaElfo= fuerzaElfo - 5}
+				}
+				else if (decisionPocima=2) {decisionPocima = 0}
+				
+				if (decisionPocima >2 || decisionPocima <=0) {} {alert ("Escribe un valor")
+				}
+
+// if (decisionPocima >2 || decisionPocima <=0) {} {alert ("Escribe un valor")} Se ha añadido como valor para evitar que escoja otro valor
+				
+//numeroAleatorio = Math.random(); (esta función sirve para agregar números aleatorios)
+//document.write(numeroAleatorio); (esta función la hemos utilizado para que nos plasme que valor es)
+			
+			
 
 			if (fuerzaOrco == fuerzaElfo){alert ("No gana nadie")}
 			if (fuerzaOrco > fuerzaElfo) {alert ("El Oco gana")}
