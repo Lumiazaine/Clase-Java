825280e9c13fafb17296a8811e2da095cd67eab1
David Luna González <lumiazaine@gmail.com>
2019-06-26 10:42:20 +0200

    Add files via upload

--- js/For/js/for.js
+++ js/For/js/for.js
@@ -1,6 +1,9 @@
 /* Vamos a crear un programa que nos salude si le damos permiso, si tienes permiso, llora.*/
 //con la notación "/*" sirve para comentar en más de una linea, se debe cerrar con "*/".
 
-var permiso:Boolean;
+var permiso;
 
+permiso=false;
 
+	if (permiso === true) {document.write("Hola, ¿Te pago el Uber?")}
+	if (permiso === false) {document.write("Sad")}
