fc7e087e3b5d663e8f6c6ea3d49028546629f305
David Luna González <lumiazaine@gmail.com>
2019-06-27 12:09:53 +0200

    Add files via upload

---
+++ js/Arrays Shift/js/shift.js
@@ -0,0 +1,10 @@
+// Dadas 3 provicncias en un Array:
+// *Eliminar la primera
+// *Eliminar de nuevo la primera
+// *Sacar el resultado en un alert
+var aProvincias = ["Burgos","Sevilla","Madrid"];
+aProvincias.shift();
+console.log(aProvincias);
+aProvincias.shift();
+console.log(aProvincias);
+alert ("Solo queda " +aProvincias)
--- js/pop/js/pop.js
+++ js/pop/js/pop.js
@@ -1,3 +1,11 @@
 // Dado un array con 4 flores
 // eliminar la última flor.
 // sacar el nombre de la flor eliminada en un alert.
+// pop por defecto borra el último valor
+var florEliminada
+var aFlores = ["Tulipanes","Margaritas","Rosas","Violetas"];
+console.log(aFlores);
+florEliminada = aFlores.pop();
+console.log(aFlores);
+console.log(florEliminada);
+alert("Se ha borrado: " + florEliminada);
