a32e7fc693c49ea37bc5944d7a626e04db9cb1ae
David Luna González <lumiazaine@gmail.com>
2019-06-27 09:07:00 +0200

    Delete for.js

--- js/For/js/for.js
+++
@@ -1,9 +0,0 @@
-/* Vamos a crear un programa que nos salude si le damos permiso, si tienes permiso, llora.*/
-// con la notación "/*" sirve para comentar en más de una linea, se debe cerrar con "*/".
-
-var permiso;
-
-permiso=false;
-
-	if (permiso === true) {document.write("Hola, ¿Te pago el Uber?")}
-	if (permiso === false) {document.write("<div>Sad</div>")}
