82a06400538b6adf5b2e5cf44788e9e835127179
David Luna González <lumiazaine@gmail.com>
2019-06-27 11:04:12 +0200

    Add files via upload

---
+++ js/Unshift/js/sem.js
@@ -0,0 +1,7 @@
+// Dado un Array de marcas de coches
+// 1)Añadir una primera marca al principio
+// 2)Sacar el resultado por la consola.
+// Push es para el final, push para el principio.
+var aCoches = ["Skoda","Mercedes","Opel","Ferrari"];
+aCoches.unshift("Renault")
+console.log(aCoches)
---
+++ js/arrays push/js/sem.js
@@ -0,0 +1,10 @@
+// dado un array con 4 frutas 
+// 1) añadid al final otra fruta
+// 2) sacar el resultado por la consola
+// Push sirve para añadir contenido
+var aFrutas = ["Fresas","Melocotones","Pera","Manzanas"];
+console.log(aFrutas);
+aFrutas.push("Melón"); 
+console.log(aFrutas);
+aFrutas.push("Platanos","Limones","Uvas");
+console.log(aFrutas);
