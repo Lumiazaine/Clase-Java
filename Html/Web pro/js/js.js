dc372aa8889a72eaf2a096fdc5e6cfcee0e1b6bb
David Luna González <lumiazaine@gmail.com>
2019-07-01 13:28:10 +0200

    Add files via upload

---
+++ Html/Web pro/js/js.js
@@ -0,0 +1,37 @@
+var escuderías={
+
+		mercedes:{
+			
+			color_linea:"#00d2be",
+			nombre_escuderia:"Mercedes",
+			bandera:"bandera_alemania.png"
+			logotipo:"logo_mercedes.jpg",
+			foto_coche:"coche_mercedes.jpg",
+			numero_podios:"162",
+			campeonatos_ganados:"5",
+			anos_campeonatos_ganados:["2014","2015","2016","2017","2018","2019"],
+			pilotos:{
+				
+				piloto1:"Lewis Hamilton",
+				piloto2:"Valtteri Bottas"
+			}
+
+
+
+
+
+		},
+		ferrari:{
+
+
+
+		},
+		redbull:{
+
+
+
+
+		},
+
+
+};
--- js/crear objeto/js/js.js
+++ js/crear objeto/js/js.js
@@ -1,14 +1,14 @@
 // crear objeto cuenta corriente con varias propiedades.
 // posteriormente recorremos el objeto listando sus propiedades en el navegador
-
+// añadir una imagen 
 
 var cuentaCorriente = {
 
-	Nombre: "Pepa",
-	Apellido: "Luna",
+	Nombre: "Peppa",
+	Apellido: "Pig",
 	Saldo:0,
 	Premium:true,
-
+	foto:"peppa-pig.jpg"
 }
 
 document.write("<h1> Ficha de cliente</h1>");
@@ -17,3 +17,5 @@ for(var datos in cuentaCorriente){
 
 	document.write(datos+ " : " + cuentaCorriente[datos] + "<br>");
 }
+
+document.write("<img src=img/" + cuentaCorriente["foto"]+">");
