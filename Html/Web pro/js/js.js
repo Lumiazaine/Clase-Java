66aea1468a0a877bfc50c860747dd10923170255
David Luna González <lumiazaine@gmail.com>
2019-07-10 13:26:22 +0200

    Add files via upload

--- Html/Web pro/js/js.js
+++ Html/Web pro/js/js.js
@@ -4,7 +4,7 @@ var escuderías={
 			
 			color_linea:"#00d2be",
 			nombre_escuderia:"Mercedes",
-			bandera:"bandera_alemania.png"
+			bandera:"bandera_alemania.png",
 			logotipo:"logo_mercedes.jpg",
 			foto_coche:"coche_mercedes.jpg",
 			numero_podios:"162",
@@ -24,36 +24,38 @@ var escuderías={
 		ferrari:{
 
 
-			color_linea:"",
-			nombre_escuderia:"",
-			bandera:""
-			logotipo:"",
-			foto_coche:"",
-			numero_podios:"",
-			campeonatos_ganados:"",
-			anos_campeonatos_ganados:[""],
+			color_linea:"#bc0000",
+			nombre_escuderia:"Ferrari",
+			bandera:"bandera_italia.jpg",
+			logotipo:"logo_ferrari.jpg",
+			foto_coche:"coche_ferrari.jpg",
+			numero_podios:"763",
+			campeonatos_ganados:"16",
+			anos_campeonatos_ganados:["1950","1953","2016","2017"],
 			pilotos:{
 				
-				piloto1:"",
-				piloto2:""
+				piloto1:"Sebastian Vettel",
+				piloto2:"Charles Leclerc"
+				}
 
 
 
 		},
 		redbull:{
 
-			color_linea:"",
-			nombre_escuderia:"",
-			bandera:""
-			logotipo:"",
-			foto_coche:"",
-			numero_podios:"",
-			campeonatos_ganados:"",
-			anos_campeonatos_ganados:[""],
+			color_linea:"#1e41ff",
+			nombre_escuderia:"Redbull",
+			bandera:"bandera_austria.gif",
+			logotipo:"logo_redbull.jpg",
+			foto_coche:"coche_redbull.jpg",
+			numero_podios:"161",
+			campeonatos_ganados:"4",
+			anos_campeonatos_ganados:["2008","2010","2015","2019"],
 			pilotos:{
 				
-				piloto1:"",
-				piloto2:""
+				piloto1:"Max Verstappen",
+				piloto2:"Pierre Gasly"
+				}
 
 
 
@@ -62,3 +64,30 @@ var escuderías={
 
 
 };
+
+
+
+// ????
+
+
+// imagen-bandera
+// nombre-escuderia
+// nombre-primer-piloto
+// nombre-segundo-piloto
+// logo-escuderias
+// podios
+// campeonatos
+
+
+var piloto1 = document.getElementById("nombre-primer-piloto");
+
+piloto1.innerHTML=;
+
+var piloto2 = document.getElementById("nombre-segundo-piloto");
+var podios = document.getElementById("podios");
+var nombreEscuderia = document.getElementById("nombre-escuderia");
+var imagenBandera= document.getElementById("imagen-bandera");
+var logoEscuderias = document.getElementById("logo-escuderia");
+var campeonatos = document.getElementById("campeonatos");
+var imagenCoche = document.getElementById("imagen-coche");
+var lineaColor= document.getElementById("imagen-coche");
